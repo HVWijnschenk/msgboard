364e278a6eed144db6cdb0a094d677aca496bfaa
Harry41348 <hvalentinow@gmail.com>
2022-10-10 09:36:20 +0100

    Database init + tailwind install

---
+++ laravel/database/seeders/VisitorSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Visitor;
+
+class VisitorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Visitor::factory()
+        ->count(20)
+        ->create();
+    }
+}

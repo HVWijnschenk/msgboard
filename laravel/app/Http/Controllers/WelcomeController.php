1ea43502eace2cfa2b1e62f9fbfc0916e719cfa3
Harry41348 <hvalentinow@gmail.com>
2022-10-17 09:25:30 +0100

    Add user seeder

--- laravel/app/Http/Controllers/WelcomeController.php
+++ laravel/app/Http/Controllers/WelcomeController.php
@@ -8,11 +8,6 @@ use App\Models\Visitor;
 class WelcomeController extends Controller {
 
   public function index() {
-    $visitors = Visitor::all();
-
-    return view('welcome',[
-        'title'=>'Our Visitors',
-        'visitors'=>$visitors
-        ]);
+    return view('welcome');
   }
 }
